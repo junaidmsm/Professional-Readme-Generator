cab21e6cf0379afe2955a9090681d550de27a0cc
BootCampTech <mjunaid@live.co.uk>
2023-02-23 13:31:31 +0000

    Syntax added to generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,107 +1,158 @@
 // Function to return a message if user doesn't want contributors
-function renderContributingSection(confirmContributers, data) {
-  if (!confirmContributers) {
-    return `
-  Thank you for your interest in helping out; however, I will not be accepting contributions from third parties.
-    `;
-  } else {
-    return `
-  ${data}
-    `;
-  }
-}
-
-// Function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) { 
-  if (license !== 'no license') {
-    return `
-  ![badge](https://img.shields.io/badge/license-${license}-blue)
-    `;
-  } else {
-    return ' ';
-  }
-}
-
-// Function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license !== 'no license') {
-  return `
-  [${license}](https://choosealicense.com/licenses/${license})
-    `;
-  } else {
-    return ' ';
-  }
-}
-
-// Function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license !== 'no license') {
-  return `
-  ## [License](#table-of-contents)
-  The application is covered under the following license:
-  ${renderLicenseLink(license)}
-    `;
-  } else {
-    return ' ';
-  }
- }
-
- // Function that returns license in table of contents
- // If there is no license, return an empty string
-function renderLicenseTOC(license) {
-  if (license !== 'no license') {
-  return `
-  * [License](#license)
-    `;
-  } else {
-    return ' ';
-  }
- }
-
-// Function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-  # ${data.title}
-  
-  ${renderLicenseBadge(data.license)}
-  ## Table-of-Contents
-  * [Description](#description)
-  * [Installation](#installation)
-  * [Usage](#usage)
-  ${renderLicenseTOC(data.license)}
-  * [Contributing](#contributing)
-  * [Tests](#tests)
-  * [Questions](#questions)
-  
-  ## [Description](#table-of-contents)
-  ${data.what}
-  ${data.why}
-  ${data.how}
-  ## [Installation](#table-of-contents)
-  ${data.installation}
-  ## [Usage](#table-of-contents)
-  ${data.usage}
-  
-  For more information on how to add screenshots for examples, visit the following website:
-  
-  [Mark Down Tutorial](https://agea.github.io/tutorial.md/)
-  
-  ${renderLicenseSection(data.license)}
-  ## [Contributing](#table-of-contents)
-  
-  ${renderContributingSection(data.confirmContributers, data.contribute)}
-  ## [Tests](#table-of-contents)
-  ${data.test}
-  ## [Questions](#table-of-contents)
-  Please contact me using the following links:
-  [GitHub](https://github.com/${data.githubUsername})
-  [Email: ${data.email}](mailto:${data.email})
-`;
-}
+  //license
+  let licenseOption = `${data.license}`;
+  let licenseLink = '';
+  //if conditional to give the license the correct link
+  if (licenseOption === 'GNU AGPLv3') {
+    licenseOption = 'GNUAGPLv3';
+    licenseLink = 'https://choosealicense.com/licenses/agpl-3.0/';
+  };
+  if (licenseOption === 'GNU GPLv3') {
+    licenseOption = 'GNUGPLv3';
+    licenseLink = 'https://choosealicense.com/licenses/gpl-3.0/';
+  };
+  if (licenseOption === 'GNU LGPLv3') {
+    licenseOption = 'GNULGPLv3';
+    licenseLink = 'https://choosealicense.com/licenses/lgpl-3.0/';
+  };
+  if (licenseOption === 'Mozilla Public License 2.0') {
+    licenseOption = 'MozillaPublicLicense2.0';
+    licenseLink = 'https://choosealicense.com/licenses/mpl-2.0/';
+  };
+  if (licenseOption === 'Apache License 2.0') {
+    licenseOption = 'ApacheLicense2.0';
+    licenseLink = 'https://choosealicense.com/licenses/apache-2.0/';
+  };
+  if (licenseOption === 'MIT License') {
+    licenseOption = 'MITLicense';
+    licenseLink = 'https://choosealicense.com/licenses/mit/';
+  };
+  if (licenseOption === 'Boost Software License 1.0') {
+    licenseOption = 'BoostSoftwareLicense1.0';
+    licenseLink = 'https://choosealicense.com/licenses/bsl-1.0/';
+  };
+  if (licenseOption === 'The Unlicense') {
+    licenseOption = 'TheUnlicense';
+    licenseLink = 'https://choosealicense.com/licenses/unlicense/';
+  };
 
-module.exports = generateMarkdown;
+//create variable to hold markdown template and dynamically add to it
+let markdownTemplate =
+    
+//title, badge, and description
+    
+`# ${data.title}
+## Description
+${data.description}
+![badge](https://img.shields.io/badge/license-${licenseOption}-brightorange)
+You can access more badges and their purposes at [shields.io](https://shields.io)
+`;
+  //create optional sections in a table of contents to 
+  //be added after required title, description
+
+let tableOfContents =
+`## Table of Contents`;
+  if (data.installation) {
+    tableOfContents +=
+      `
+  * [Installation](#installation)`
+  };
+  if (data.instructions) {
+    tableOfContents +=
+      `
+  * [Usage](#usage)`
+  };
+  if (data.contribution) {
+    tableOfContents +=
+      `
+  * [Contribution](#contribution)`
+  };
+  if (data.testing) {
+    tableOfContents +=
+      `
+  * [Testing](#testing)`
+  };
+
+  //append table to template
+  markdownTemplate += tableOfContents;
+
+  //add contact info and license (required)
+  markdownTemplate +=
+    `
+  * [Questions](#questions)`;
+  markdownTemplate +=
+    `
+  * [License](#license)
+    
+    `;
+
+  //add optional sections content
+
+  //installation
+  if (data.installation) {
+    markdownTemplate +=
+      `
+## Installation
+    
+  _Follow these steps to properly install this application:_
+  ${data.installation}`
+  };
+
+  //usage
+  if (data.instructions) {
+    markdownTemplate +=
+      `
+      
+## Usage
+  _Instructions for use:_
+  ${data.instructions}`
+  };
+
+  //contributions
+  if (data.contribution) {
+    markdownTemplate +=
+      `
+      
+## Contribution
+  _If you would like to contribute, please adhere to these guidelines:_
+  ${data.contribution}`
+  };
+
+  //testing
+  if (data.testing) {
+    markdownTemplate +=
+      `
+      
+## Testing
+  _Instructions for testing application:_
+  ${data.testing}`
+  };
+
+  //questions
+    markdownTemplate +=
+      `
+      
+## Questions
+      
+  _For further questions:_
+  ${data.questions}
+  
+  _Contact Info:_
+  GitHub: [${data.username}](https://github.com/${data.username})
+  Email: [${data.email}](mailto:${data.email})`;
+  
+  markdownTemplate +=
+    `
+    
+## License
+      
+  _This application has the ${data.license}._
+      
+  For more information please view the [license description](${licenseLink}).
+  
+  `;
+  return markdownTemplate;
+}
 
 module.exports = generateMarkdown;
